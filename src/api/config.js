5159746b0c0c49cd19b6ff1e1453457955d55817
XiaoCheng123 <1197138309@qq.com>
2017-12-13 19:32:23 +0800

    添加获取推荐页面的轮播图数据

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,16 @@
 /**
  * Created by admin on 2017/12/12.
  */
+export const commonParams = {
+  g_tk: 5381,
+  format: 'jsonp',
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
---
+++ src/api/recommend.js
@@ -0,0 +1,17 @@
+/**
+ * Created by admin on 2017/12/12.
+ */
+import {commonParams, options} from './config'
+import jsonp from 'common/js/jsonp'
+
+export function getRecommend() {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+
+  return jsonp(url, data, options)
+}
