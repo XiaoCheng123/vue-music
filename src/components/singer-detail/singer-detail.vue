01327d19862d65a5e03bb513654e57a19a807c26
XiaoCheng123 <1197138309@qq.com>
2018-01-26 13:40:41 +0800

    为子路由切换添加动画

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,9 +1,29 @@
 <template>
+  <transition name="slide">
+    <div class="singer-detail">
+    </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
   export default {}
 </script>
 
-<style scoped>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .singer-detail
+    position: fixed
+    z-index: 100
+    background: $color-background
+    top: 0
+    bottom: 0
+    right: 0
+    left: 0
+
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
 </style>
